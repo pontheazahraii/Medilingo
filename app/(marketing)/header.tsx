5322e78a38c5dec17530bd8cd04cee8cc2d2f367
Ponthea Zahraii <pontheazahraii@gmail.com>
2025-04-26 18:47:21 -0700

    remove banner

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -25,68 +25,64 @@ export const Header = () => {
   const { isSignedIn } = useAuth();
   const [hideBanner, setHideBanner] = useState(true);
 
-  return (
-    <>
-      {/* <Banner hide={hideBanner} setHide={setHideBanner} /> */}
+    return (<></>);
+  // return (
+  //   <>
+  //     <Banner hide={hideBanner} setHide={setHideBanner} />
 
-      <header
-        className={cn(
-          "h-20 w-full border-b-2 border-slate-200 px-4",
-          !hideBanner ? "mt-20 sm:mt-16 lg:mt-10" : "mt-0"
-        )}
-      >
-        <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
-          <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
-            <Image src="/Pete_Alone_Logo.png" alt="Mascot" height={40} width={40} />
+  //     <header
+  //       className={cn(
+  //         "h-20 w-full border-b-2 border-slate-200 px-4",
+  //         !hideBanner ? "mt-20 sm:mt-16 lg:mt-10" : "mt-0"
+  //       )}
+  //     >
+  //       <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
+  //         <Link href="/" className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
+  //           <Image src="/Pete_Alone_Logo.png" alt="Mascot" height={40} width={40} />
 
-            <h1 className="text-2xl font-extrabold tracking-wide text-blue-500">
-              Medilingo
-            </h1>
-          </Link>
+  //           <h1 className="text-2xl font-extrabold tracking-wide text-blue-500">
+  //             Medilingo
+  //           </h1>
+  //         </Link>
 
-          <div className="flex gap-x-3">
-            <ClerkLoading>
-              <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
-            </ClerkLoading>
-            <ClerkLoaded>
-              <SignedIn>
-                <div className="flex items-center gap-x-3">
-                  <UserButton afterSignOutUrl="/" />
-                  <div className="hidden sm:block">
-                    <LogoutButton compact />
-                  </div>
-                </div>
-              </SignedIn>
+  //         <div className="flex gap-x-3">
+  //           <ClerkLoading>
+  //             <Loader className="h-5 w-5 animate-spin text-muted-foreground" />
+  //           </ClerkLoading>
+  //           <ClerkLoaded>
+  //             <SignedIn>
+  //               <UserButton afterSignOutUrl="/" />
+  //             </SignedIn>
 
-              <SignedOut>
-                <SignInButton
-                  mode="modal"
-                  afterSignInUrl="/learn"
-                  afterSignUpUrl="/learn"
-                >
-                  <Button size="lg" variant="ghost">
-                    Login
-                  </Button>
-                </SignInButton>
-              </SignedOut>
+  //             <SignedOut>
+  //               <SignInButton
+  //                 mode="modal"
+  //                 afterSignInUrl="/learn"
+  //                 afterSignUpUrl="/learn"
+  //               >
+  //                 <Button size="lg" variant="ghost">
+  //                   Login
+  //                 </Button>
+  //               </SignInButton>
+  //             </SignedOut>
 
-              <Link
-                href={links.sourceCode}
-                target="_blank"
-                rel="noreferrer noopener"
-                className={isSignedIn ? "pt-1.5" : "pt-3"}
-              >
-                <Image
-                  src="/github.svg"
-                  alt="Source Code"
-                  height={20}
-                  width={20}
-                />
-              </Link>
-            </ClerkLoaded>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+  //             <Link
+  //               href={links.sourceCode}
+  //               target="_blank"
+  //               rel="noreferrer noopener"
+  //               className={isSignedIn ? "pt-1.5" : "pt-3"}
+  //             >
+  //               <Image
+  //                 src="/github.svg"
+  //                 alt="Source Code"
+  //                 height={20}
+  //                 width={20}
+  //               />
+  //             </Link>
+  //           </ClerkLoaded>
+  //         </div>
+  //       </div>
+  //     </header>
+  //   </>
+  // );
 };
